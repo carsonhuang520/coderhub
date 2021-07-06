3a4753467ca002e36d8189c648221fdff4776007
carsonhuang <981347807@qq.com>
2021-07-07 01:11:05 +0800

    初步完成登录接口及密码验证

--- src/app/error-handle.js
+++ src/app/error-handle.js
@@ -11,6 +11,14 @@ const errorHandler = (error, ctx) => {
       status = 409
       message = '用户名已经存在~'
       break
+    case errorTypes.USER_DOES_NOT_EXISTS:
+      status = 400
+      message = '用户名不存在~'
+      break
+    case errorTypes.PASSWORD_IS_INCORRECT:
+      status = 400
+      message = '密码错误~'
+      break
     default:
       status = 404
       message = 'NOT FOUND'
---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,27 @@
+const errorTypes = require('../constants/error-types')
+const userService = require('../service/user.service')
+const { md5password } = require('../utils/password-handle')
+
+const verifyLogin = async (ctx, next) => {
+  const { name, password } = ctx.request.body
+
+  if (!name || !password) {
+    const error = new Error(errorTypes.NAME_OR_PASSWORD_IS_REQUIRED)
+    return ctx.app.emit('error', error, ctx)
+  }
+
+  const result = await userService.getUserByName(name)
+  const user = result[0]
+  if (!user) {
+    const error = new Error(errorTypes.USER_DOES_NOT_EXISTS)
+    return ctx.app.emit('error', error, ctx)
+  }
+
+  if (md5password(password) !== user.password) {
+    const error = new Error(errorTypes.PASSWORD_IS_INCORRECT)
+    return ctx.app.emit('error', error, ctx)
+  }
+  await next()
+}
+
+module.exports = { verifyLogin }
