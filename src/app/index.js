fea076f2bd9f4f7bd25aeaa687614e9b46ab6cf1
carsonhuang <981347807@qq.com>
2021-07-07 14:52:51 +0800

    封装useRoutes

--- src/app/index.js
+++ src/app/index.js
@@ -1,20 +1,15 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 
-const userRouter = require('../router/user.router')
-const authRouter = require('../router/auth.router')
-const momentRouter = require('../router/moment.router')
 const errorHandler = require('./error-handle')
+const useRoutes = require('../router')
 
 const app = new Koa()
 
+app.useRoutes = useRoutes
+
 app.use(bodyParser())
-app.use(userRouter.routes())
-app.use(userRouter.allowedMethods())
-app.use(authRouter.routes())
-app.use(authRouter.allowedMethods())
-app.use(momentRouter.routes())
-app.use(momentRouter.allowedMethods())
+app.useRoutes()
 
 app.on('error', errorHandler)
 
